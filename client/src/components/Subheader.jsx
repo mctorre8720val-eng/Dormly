e8449b16796eaf805987027972841436d03b7d07
kel.SE <mctorre8720val@student.fatima.edu.ph>
2026-06-17 14:23:32 +0800

    vercel v2

--- client/src/components/Subheader.jsx
+++ client/src/components/Subheader.jsx
@@ -1,20 +1,26 @@
 function Subheader({ Header, Subheader }) {
+
+    const isMobile = window.innerWidth <= 430;
+
     return (
         <div 
             className="Subheader"
             style={{
-                paddingLeft: "12rem",
-                paddingRight: "12rem"
+                paddingLeft: isMobile ? "1rem" : "12rem",
+                paddingRight: isMobile ? "1rem" : "12rem",
+                textAlign: isMobile ? "center" : "left"
             }}
         >
+
             <h1 
                 className="Header"
                 style={{
-                    fontSize: "2rem"
+                    fontSize: isMobile ? "1.6rem" : "2rem"
                 }}
             >
                 {Header}
             </h1>
+
 
             <p 
                 className="Subheader"
